17e95c8c945c5309d2a767d6dd049eff419a22d9
PavelYasukevich <ptntbox@gmail.com>
2020-09-10 11:46:16 +0500

    showoff stage

--- posts/views.py
+++ posts/views.py
@@ -3,13 +3,11 @@ import datetime as dt
 from django.contrib.auth.decorators import login_required
 from django.core.paginator import Paginator
 from django.shortcuts import get_object_or_404, redirect, render
-from django.views.decorators.cache import cache_page
 
 from .forms import CommentForm, PostForm
 from .models import Follow, Group, Post, User
 
 
-@cache_page(20)
 def index(request):
     post_list = Post.objects.all()
     paginator = Paginator(post_list, 10)
@@ -130,7 +128,12 @@ def add_comment(request, username, post_id):
         new_comment.author = request.user
         new_comment.post = post
         new_comment.save()
-    return redirect("post_view", username=author.username, post_id=post.id)
+        return redirect("post_view", username=author.username, post_id=post.id)
+    return render(
+        request,
+        "posts/view_post.html",
+        {"form": form, "author": author, "post": post, "items": comments},
+    )
 
 
 @login_required
